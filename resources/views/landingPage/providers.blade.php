6c7edabecf58561e0c523518a7ec920e53437efd
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-05-24 13:31:19 +0300

    complete caregiver page

--- resources/views/landingPage/providers.blade.php
+++ resources/views/landingPage/providers.blade.php
@@ -1,3 +1,19 @@
 <x-layoutcomponents.layout>
-    <p>Providers page</p>
+    <div class="container grid mx-auto p-8 gap-3 md:grid-cols-2">
+        <section class="self-center">
+            <img src="{{ asset('images/care5.webp') }}" alt="" class="rounded">
+        </section>
+        <section class="space-y-3 self-center">
+            <h3 class="text-orange-400 font-bold md:text-lg lg:text-2xl">Hire with confidence, we'll handle the rest.</h3>
+            <p>Looking to recruit a proficient caregiver for open job position in your agency.we connect you with
+                several caregivers from diffrent places.How you go about hiring careproviders will partially depend on
+                what kind of help you need. Make sure to have credential to verify that you are a certified caregiving
+                agency. Job available and requirements need for one to qualify for hire. Advertise your job posting for
+                applicants on our page to apply. View applicants &amp; connect with the qualified candidate.</p>
+            <div>
+                <x-navbarcomponents.signup-link class="rounded py-1" />
+            </div>
+        </section>
+    </div>
+
 </x-layoutcomponents.layout>
